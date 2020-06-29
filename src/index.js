46365c743570e368bc5858eafe80a42aeae441dd
duytd20 <duytd20@DUYTD20.local>
2020-06-29 13:35:04 +0700

    upload project

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {Provider} from 'react-redux';
+import configureStore from './store/configureStore';
+import MyApp from './MyApp';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default class App extends Component {
+  constructor() {
+    super();
+    Icon.loadFont();
+    this.state = {
+      isLoading: true,
+      store: configureStore(() => this.setState({isLoading: false})),
+    };
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return null;
+    }
+
+    return (
+      <Provider store={this.state.store}>
+        <MyApp />
+      </Provider>
+    );
+  }
+}
